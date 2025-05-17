a28cf584cd34cb24003d5a8339ffefaabbff461b
Nikhil <nikhilgupta932003@gmail.com>
2025-05-18 01:31:41 +0530

    added some changes

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
+    await client.connect(MONGO_URL);
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("docker-learner");
     const data = await db.collection('users').find({}).toArray();
     
     client.close();
